82e321026be80632f5d61b486dc789bffbbae241
jorgejlrr <jorge14.jlrr@gmail.com>
2019-05-13 17:42:58 -0500

    Se agrega INDEX - CLIENTE

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -1,4 +1,39 @@
 @extends('plantilla.plantilla')
 @section('contenido')
-
+<section class="content-header">
+  <h1>
+    CLIENTES
+  </h1>
+</section>
+<section class="content">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="box">
+                <div class="box-header">
+                    <a href="{{url('cliente/create')}}" class="btn btn-primary">Registrar Cliente</a>
+                </div>
+                <div class="box-body">
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                            <th>DNI</th>
+                            <th>Apellidos</th>
+                            <th>Nombres</th>
+                            <th>Direccion</th>
+                            <th>Opciones</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
