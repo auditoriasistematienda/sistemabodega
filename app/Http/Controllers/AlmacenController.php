1598c051be8395f823134bc76fae37ad0670a131
RedJohn1426 <alex98rodriguezromero@gmail.com>
2019-05-13 16:41:48 -0500

    Se agrega almacen - modelo y controlador

---
+++ app/Http/Controllers/AlmacenController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace SistemaGlobal\Http\Controllers;
+
+use SistemaGlobal\Almacen;
+use Illuminate\Http\Request;
+
+class AlmacenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view ('almacen.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \SistemaGlobal\Almacen  $almacen
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Almacen $almacen)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \SistemaGlobal\Almacen  $almacen
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Almacen $almacen)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \SistemaGlobal\Almacen  $almacen
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Almacen $almacen)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \SistemaGlobal\Almacen  $almacen
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Almacen $almacen)
+    {
+        //
+    }
+}
