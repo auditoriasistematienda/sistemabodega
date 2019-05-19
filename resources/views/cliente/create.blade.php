b75ed24b8f837ddbd02e3270dd53abc625dec7e1
RedJohn1426 <alex98rodriguezromero@gmail.com>
2019-05-18 21:10:06 -0500

    Se agrega dependencias y modifica create cliente

--- resources/views/cliente/create.blade.php
+++ resources/views/cliente/create.blade.php
@@ -6,52 +6,70 @@
   </h1>
 </section>
 <section class="content">
-    <div class="row">
-        <div class="box box-primary">
-            <div class="box-header with-border">
-                <h4 class="box-title">Agregar Cliente</h4>
-            </div>
-            <form action="">
-                <div class="box-body">
-                    <div class="form-group col-md-6">
-                        <label for="">DNI</label>
-                        <input type="text" class="form-control">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="">Apellidos</label>
-                        <input type="text" class="form-control">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="">Nombres</label>
-                        <input type="text" class="form-control">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="">Direccion</label>
-                        <input type="text" class="form-control">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="">Telefono</label>
-                        <input type="text" class="form-control">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="">Sexo</label>
-                        <select name="" id="" class="form-control"></select>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="">Fecha Nacimiento</label>
-                        <input type="date" class="form-control">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="">Email</label>
-                        <input type="email" class="form-control">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="">Tipo de Usuario</label>
-                        <select name="" id="" class="form-control"></select>
-                    </div>
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h4 class="box-title">Agregar Cliente</h4>
                 </div>
-            </form>
-        </div>
-    </div>
+                <form role="form" action="">
+                    <div class="box-body">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="">DNI</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Direccion</label>
+                                    <input type="text" class="form-control">
+                                </div>
+{{--                                <div class="form-group">--}}
+{{--                                    <div class="input-group date"></div>--}}
+{{--                                    <label for="">Fecha Nacimiento</label>--}}
+{{--                                    <input type="text" class="form-control pull-right" id="datepicker">--}}
+{{--                                </div>--}}
+                                <div class="form-group">
+                                    <label>Fecha de Nacimiento:</label>
+                                    <div class="input-group date">
+                                        <div class="input-group-addon">
+                                            <i class="fa fa-calendar"></i>
+                                        </div>
+                                        <input type="text" class="form-control pull-right" id="datepicker">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="">Apellidos</label>
+                                    <input type="text" class="form-control">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="">Telefono</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Sexo</label>
+                                    <select name="" id="" class="form-control"></select>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="">Nombres</label>
+                                    <input type="text" class="form-control">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="">Email</label>
+                                    <input type="email" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Tipo de Usuario</label>
+                                    <select name="" id="" class="form-control"></select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
 </section>
 @endsection
--- resources/views/plantilla/plantilla.blade.php
+++ resources/views/plantilla/plantilla.blade.php
@@ -15,6 +15,20 @@
     <!-- Theme style -->
     <link rel="stylesheet" href="{{asset('plantilla/dist/css/adminlte.min.css')}}">
     <link rel="stylesheet" href="{{asset('plantilla/dist/css/skins/skin-blue.min.css')}}">
+
+    <!-- daterange picker -->
+    <link rel="stylesheet" href="{{asset('plantilla/bower_components/bootstrap-daterangepicker/daterangepicker.css')}}">
+    <!-- iCheck for checkboxes and radio inputs -->
+    <link rel="stylesheet" href="{{asset('plantilla/plugins/iCheck/all.css')}}">
+    <!-- Bootstrap Color Picker -->
+    <link rel="stylesheet" href="{{asset('plantilla/bower_components/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css')}}">
+    <!-- Bootstrap time Picker -->
+    <link rel="stylesheet" href="{{asset('plantilla/plugins/timepicker/bootstrap-timepicker.min.css')}}">
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{asset('plantilla/bower_components/select2/dist/css/select2.min.css')}}">
+    <!-- bootstrap datepicker -->
+    <link rel="stylesheet" href="{{asset('plantilla/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css')}}">
+
     <!-- Google Font -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
@@ -166,7 +180,7 @@
                 <a href="https://ucv.edu.pe/" class="btn-lg" target="_blank"><i class="fa fa-university"></i></a>
             </div>
             <!-- Default to the left -->
-            <strong>Copyright &copy; 2019 <a href="" target="_blank">AdminLTE</a>.</strong> All rights reserved.
+            <strong>Copyright &copy; 2019 <a href="https://github.com/globalauditoria" target="_blank">Alumnos de Auditorio y Seguridad de la Información</a>.</strong> All rights reserved.
         </footer>
 
         <!-- Control Sidebar -->
@@ -189,5 +203,115 @@
     <!-- AdminLTE App -->
     <script src="{{asset('plantilla/dist/js/adminlte.min.js')}}"></script>
 
+    <!-- FastClick -->
+    <script src="{{asset('plantilla/bower_components/fastclick/lib/fastclick.js')}}"></script>
+
+    <!-- Sparkline -->
+    <script src="{{asset('plantilla/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js')}}"></script>
+    <!-- jvectormap  -->
+    <script src="{{asset('plantilla/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
+    <script src="{{asset('plantilla/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
+    <!-- SlimScroll -->
+    <script src="{{asset('plantilla/bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
+    <!-- ChartJS -->
+    <script src="{{asset('plantilla/bower_components/chart.js/Chart.js')}}"></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="{{asset('plantilla/dist/js/pages/dashboard2.js')}}"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="{{asset('plantilla/dist/js/demo.js')}}"></script>
+
+    <!-- Select2 -->
+    <script src="{{asset('plantilla/bower_components/select2/dist/js/select2.full.min.js')}}"></script>
+    <!-- InputMask -->
+    <script src="{{asset('plantilla/plugins/input-mask/jquery.inputmask.js')}}"></script>
+    <script src="{{asset('plantilla/plugins/input-mask/jquery.inputmask.date.extensions.js')}}"></script>
+    <script src="{{asset('plantilla/plugins/input-mask/jquery.inputmask.extensions.js')}}"></script>
+
+    <!-- date-range-picker -->
+    <script src="{{asset('plantilla/bower_components/moment/min/moment.min.js')}}"></script>
+    <script src="{{asset('plantilla/bower_components/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
+    <!-- bootstrap datepicker -->
+    <script src="{{asset('plantilla/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js')}}"></script>
+    <!-- bootstrap color picker -->
+    <script src="{{asset('plantilla/bower_components/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js')}}"></script>
+    <!-- bootstrap time picker -->
+    <script src="{{asset('plantilla/plugins/timepicker/bootstrap-timepicker.min.js')}}"></script>
+    <!-- SlimScroll -->
+    <script src="{{asset('plantilla/bower_components/jquery-slimscroll/jquery.slimscroll.min.js')}}"></script>
+    <!-- iCheck 1.0.1 -->
+    <script src="{{asset('plantilla/plugins/iCheck/icheck.min.js')}}"></script>
+
+    })
+    <script>
+        $(function () {
+            //Initialize Select2 Elements
+            $('.select2').select2()
+
+            //Datemask dd/mm/yyyy
+            $('#datemask').inputmask('dd/mm/yyyy', {'placeholder': 'dd/mm/yyyy'})
+            //Datemask2 mm/dd/yyyy
+            $('#datemask2').inputmask('mm/dd/yyyy', {'placeholder': 'mm/dd/yyyy'})
+            //Money Euro
+            $('[data-mask]').inputmask()
+
+            //Date range picker
+            $('#reservation').daterangepicker()
+            //Date range picker with time picker
+            $('#reservationtime').daterangepicker({
+                timePicker: true,
+                timePickerIncrement: 30,
+                format: 'MM/DD/YYYY h:mm A'
+            })
+            //Date range as a button
+            $('#daterange-btn').daterangepicker(
+                {
+                    ranges: {
+                        'Today': [moment(), moment()],
+                        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                        'This Month': [moment().startOf('month'), moment().endOf('month')],
+                        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                    },
+                    startDate: moment().subtract(29, 'days'),
+                    endDate: moment()
+                },
+                function (start, end) {
+                    $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
+                }
+            )
+
+            //Date picker
+            $('#datepicker').datepicker({
+                autoclose: true
+            })
+
+            //iCheck for checkbox and radio inputs
+            $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+                checkboxClass: 'icheckbox_minimal-blue',
+                radioClass: 'iradio_minimal-blue'
+            })
+            //Red color scheme for iCheck
+            $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
+                checkboxClass: 'icheckbox_minimal-red',
+                radioClass: 'iradio_minimal-red'
+            })
+            //Flat red color scheme for iCheck
+            $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
+                checkboxClass: 'icheckbox_flat-green',
+                radioClass: 'iradio_flat-green'
+            })
+
+            //Colorpicker
+            $('.my-colorpicker1').colorpicker()
+            //color picker with addon
+            $('.my-colorpicker2').colorpicker()
+
+            //Timepicker
+            $('.timepicker').timepicker({
+                showInputs: false
+            })
+        })
+    </script>
 </body>
 </html>
