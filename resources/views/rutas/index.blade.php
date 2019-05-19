276551addc8409172427a10e967a90db5ffdd9fb
jorgejlrr <jorge14.jlrr@gmail.com>
2019-05-18 19:49:32 -0500

    se agrega table rutas

--- resources/views/rutas/index.blade.php
+++ resources/views/rutas/index.blade.php
@@ -1,5 +1,45 @@
 @extends('plantilla.plantilla')
 @section('contenido')
-
+<section class="content-header">
+  <h1>
+    RUTAS
+  </h1>
+</section>
+<section class="content">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="box">
+                <div class="box-header">
+                    <a href="{{url('rutas/create')}}" class="btn btn-primary">Agregar Ruta</a>
+                </div>
+                <div class="box-body">
+                    <table id="myTable" class="table table-bordered table-hover dataTable">
+                        <thead>
+                            <th>Transporte</th>
+                            <th>Cliente</th>
+                            <th>Fecha Salida</th>
+                            <th>Fecha Retorno</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
 
+@section('scripts')
+<script>
+$(document).ready( function () {
+    $('#myTable').DataTable();
+} );
+</script>
+@endsection
