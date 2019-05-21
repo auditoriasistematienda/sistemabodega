2deabfdf7fdfe956be8d6c8077a3b805ad0b8eb1
jorgejlrr <jorge14.jlrr@gmail.com>
2019-05-20 20:03:31 -0500

    new bd

--- bdsistemaglobal.sql
+++ bdsistemaglobal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-05-2019 a las 02:54:47
+-- Tiempo de generación: 21-05-2019 a las 03:02:54
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.3
 
